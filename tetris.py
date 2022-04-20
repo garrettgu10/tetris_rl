e07f09b5bf9d12c00ff1b5650e8f7fca84270569
Garrett Gu <gu@utexas.edu>
2022-04-20 16:06:15 -0500

    more stuff

--- tetris.py
+++ tetris.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
 import enum
+import random
 from typing import List, Tuple, Set
+from collections import deque
 
 class PieceColor(enum.Enum):
     YELLOW = 93
@@ -9,6 +12,7 @@ class PieceColor(enum.Enum):
     BLUE = 34
     GREEN = 32
     RED = 31
+    EMPTY = 0
 
     def ansi_code(color: int) -> str:
         return f"\033[{color}m"
@@ -31,7 +35,7 @@ class PieceShape(object):
                     print("-", end="")
             print()
     
-    def rotate(self, rotation: Rotation):
+    def rotate(self, rotation: Rotation) -> PieceShape:
         new_points = set()
         if rotation == Rotation.CCW:
             for point in self.points:
@@ -46,28 +50,82 @@ class PieceShape(object):
             
             return PieceShape(self.size, new_points)
 
-class PieceType(object):
-    def __init__(self, color: PieceColor, shape: PieceShape):
+class Piece(object):
+    def __init__(self, color: PieceColor, shape: PieceShape, name: str = "No name"):
         self.color = color
         self.shape = shape
+        self.name = name
 
     def print(self):
         print(PieceColor.ansi_code(self.color.value))
         self.shape.print()
     
     def rotate(self, rotation: Rotation):
-        return PieceType(self.color, self.shape.rotate(rotation))
+        return Piece(self.color, self.shape.rotate(rotation), self.name)
 
-pieces = [
-    PieceType(PieceColor.YELLOW, PieceShape(2, {(0, 0), (0, 1), (1, 0), (1, 1)})),
-    PieceType(PieceColor.CYAN, PieceShape(4, {(0, 2), (1, 2), (2, 2), (3, 2)})),
-    PieceType(PieceColor.PURPLE, PieceShape(3, {(0, 1), (1, 1), (2, 1), (1, 2)})),
-    PieceType(PieceColor.ORANGE, PieceShape(3, {(0, 1), (1, 1), (2, 1), (2, 2)})),
-    PieceType(PieceColor.BLUE, PieceShape(3, {(0, 1), (1, 1), (2, 1), (0, 2)})),
-    PieceType(PieceColor.GREEN, PieceShape(3, {(0, 1), (1, 1), (1, 2), (2, 2)})),
-    PieceType(PieceColor.RED, PieceShape(3, {(0, 2), (1, 2), (1, 1), (2, 1)})),
-]
+default_pieces = {
+    "O": Piece(PieceColor.YELLOW, PieceShape(2, {(0, 0), (0, 1), (1, 0), (1, 1)})),
+    "I": Piece(PieceColor.CYAN, PieceShape(4, {(0, 2), (1, 2), (2, 2), (3, 2)})),
+    "T": Piece(PieceColor.PURPLE, PieceShape(3, {(0, 1), (1, 1), (2, 1), (1, 2)})),
+    "L": Piece(PieceColor.ORANGE, PieceShape(3, {(0, 1), (1, 1), (2, 1), (2, 2)})),
+    "J": Piece(PieceColor.BLUE, PieceShape(3, {(0, 1), (1, 1), (2, 1), (0, 2)})),
+    "S": Piece(PieceColor.GREEN, PieceShape(3, {(0, 1), (1, 1), (1, 2), (2, 2)})),
+    "Z": Piece(PieceColor.RED, PieceShape(3, {(0, 2), (1, 2), (1, 1), (2, 1)})),
+}
 
-for piece in pieces:
+for piece_name in default_pieces.keys():
+    default_pieces[piece_name].name = piece_name
+
+#represents only the matrix, not next pieces, swap pieces, current piece
+class Board(object):
+    def __init__(self):
+        self.board = [[PieceColor.EMPTY] * 10 for _ in range(40)]
+
+    def check_collision(self, piece: Piece, x: int, y: int) -> bool:
+        for point in piece.shape.points:
+            px = x + point[0]
+            py = y + point[1]
+            if px < 0 or px >= 10 or py < 0 or py >= 40:
+                return True
+            
+            if self.board[py][px][0] != PieceColor.EMPTY:
+                return True
+        return False
+
+    # places a piece; assumes piece is in a valid position (no collision and not floating)
+    def freeze(self, piece:Piece, x: int, y:int):
+        for point in piece.shape.points:
+            px = x + point[0]
+            py = y + point[1]
+            self.board[py][px] = piece.color
+
+class Game(object):
+    def __init__(self):
+        self.board = Board()
+        self.bag = deque()
+        self._refill_bag()
+
+        self.set_curr_piece(self._next_piece())
+
+        self.swap_piece = None
+        self.game_over = False
+    
+    def _refill_bag(self):
+        while len(self.bag) <= len(default_pieces.keys()):
+            self.bag.append(random.shuffle(list(default_pieces.values())))
+    
+    def _next_piece(self) -> Piece:
+        self._refill_bag()
+        return self.bag.popleft()
+
+    def _set_curr_piece(self, piece: Piece):
+        self.curr_piece = piece
+        self.curr_piece_x = 5 - (piece.size + 1) // 2
+        self.curr_piece_y = 20 if piece.size == 2 else 19
+
+        if self.board.check_collision(self.curr_piece, self.curr_piece_x, self.curr_piece_y):
+            self.game_over = True
+
+for piece in default_pieces.values():
     piece.rotate(Rotation.CCW).print()
     piece.print()
